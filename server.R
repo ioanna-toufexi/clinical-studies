fe3691cfda182a5d541b65e817168e9b8a33b626
ioanna-toufexi <ioanna.toufexi@city.ac.uk>
2020-04-16 16:19:40 +0100

    added choice of variable on the ui, another plot on the server

--- server.R
+++ server.R
@@ -6,16 +6,40 @@ function(input, output) {
   
   studies <- get_studies()
   
+  type <- reactive({
+    input$type
+  })
+  
   output$plot <- renderPlot({
     
-    studies %>% group_by(StartMonth) %>%
-      summarise(count = n()) %>% 
+    if(type()=="StartDate") {
+      studies %>% group_by_at(vars(type())) %>%
+        summarise(count = n()) %>% 
+        ggplot() +
+        geom_bar(aes_string(x = type(),
+                            y = "count"), fill="blue", stat = "identity") +
+        theme_minimal() +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+        labs(y= "number of studies", x = "month (2020)", title = "Number of clinical studies related to COVID-19 per starting month (from Jan 2020)")
+    }
+    else {
+      
+      #Some observations have multiple countries. 
+      #Data manipulations to separate them.
+      countries_as_string = paste(studies$Country, collapse = ',,')
+      countries <- data.frame(Country = unlist(strsplit(countries_as_string, split=",,")))
+      
+      countries %>% 
+        group_by(Country) %>% 
+        summarise(count = n()) %>% 
+        top_n(20, count) %>% 
+        mutate(highlight_flag = ifelse(Country != 'No data', T, F)) %>% 
       ggplot() +
-      geom_bar(aes(x = StartMonth,
-                   y = count), fill="blue", stat = "identity") +
-      theme_minimal() +
-      theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-      labs(y= "number of studies", x = "month (2020)", title = "Number of clinical studies related to COVID-19 per starting month (2020)")
+        geom_bar(aes(x = reorder(Country, -count),
+                     y = count, fill=highlight_flag), stat = "identity") +
+        theme_minimal() +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.position="none") +
+        labs(y= "number of studies", x = "Country", title = "Top 20 countries per number of clinical studies related to COVID-19 (from Jan 2020). \nIncludes appearances in multi-country collaborations.")}
   })
   
 }
--- ui.R
+++ ui.R
@@ -15,6 +15,12 @@ fluidPage(
     p("2) \"COVID-19\" is used as a generic search keyword, 
     thus the number is slightly higher than the condition-specific search on ClinicalTrials.gov."),
     p("3) This dataset only contains studies listed on ClinicalTrials.gov. For additional studies refer to https://www.who.int/ictrp/en/"),
+  ),
+    sidebarPanel(
+      radioButtons(
+        inputId='type', label='Choose variable', choiceNames=list("Start Date", "Country"), choiceValues=list("StartDate", "Country")
+      )
+    ),
     plotOutput('plot')
-  )
+    
 )
